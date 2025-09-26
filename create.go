08323f145156d2050438290933bb59b8347c721c
Pascal Klodel <99654847+KiroLakestrike@users.noreply.github.com>
2025-09-26 15:22:08 +0000

    Added some code on the trainride home.

--- create.go
+++ create.go
@@ -1,7 +1,24 @@
 // Create new empty files with this
-
 package fileutil
 
-func create(String destination, fileName) bool, error {
-  // File creation code here. 
+import(
+  "errors"
+  "os"
+  "path/filepath"
+  "fmt"
+)
+
+func create(path string, fileName string) (bool, error) {
+  // Will create an empty file with the given extension under the given path.
+  // If file already Exists, the function will stop with a error message "File already exists"
+  // Returns true and nil, if the file ws created successfully, and false and error if the file was not created
+
+  fullPath := filepath.Join(path, fileName)
+
+  // Checks if the file already exists
+  if _, err := os.Stat(fullPath); err == nil {
+    err := fmt.Errorf("File %v already exists", fileName)
+    return false, err
+
+  }
 }
