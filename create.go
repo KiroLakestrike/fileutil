3481fcd5836bb636f2c6981e68b2da1960f258ea
Pascal Klodel <99654847+KiroLakestrike@users.noreply.github.com>
2025-09-26 15:54:35 +0000

    finished the create function.

--- create.go
+++ create.go
@@ -2,7 +2,6 @@
 package fileutil
 
 import(
-  "errors"
   "os"
   "path/filepath"
   "fmt"
@@ -19,6 +18,19 @@ func create(path string, fileName string) (bool, error) {
   if _, err := os.Stat(fullPath); err == nil {
     err := fmt.Errorf("File %v already exists", fileName)
     return false, err
-
+  } else if !os.IsNotExist(err) {
+    err := fmt.Errorf("Another error occured when checking if the file %v exists", fileName)
+    return false, err
   }
+
+  // Create the new file
+  file, err := os.OpenFile(fullPath, os.O_CREATE|os.O_WRONLY|os.O_EXCL, 0644)
+  if err != nil {
+    err := fmt.Errorf("There was an error creating the file %v", fileName)
+    return false, err
+  }
+  defer file.Close()
+
+  // everything went well
+  return true, nil
 }
